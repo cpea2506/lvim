f57211424aeb8c22e306c596ecba30d135cfcc3e
cpea2506 <vienpro2506@gmail.com>
2021-12-28 03:24:16 +0700

    add more client id options

--- lua/pea/config/presence.lua
+++ lua/pea/config/presence.lua
@@ -7,12 +7,21 @@ M.config = function()
         return
     end
 
+    local client_id = {
+        vs_code = "383226320970055681",
+        coc_nvim = "768090036633206815",
+        pea_vim = "914799712794705961",
+    }
+
     presence:setup {
         auto_update = true,
         neovim_image_text = "NeoVim, not Vim pls!",
         main_image = "file",
-        client_id = "914799712794705961",
+        client_id = client_id.pea_vim,
         buttons = true,
+        workspace_text = function()
+            return "Rustacean"
+        end,
     }
 end
 
