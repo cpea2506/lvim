240b9b5f0fe979bb63de9c3c8acd07dffde4eb3e
cpea2506 <vienpro2506@gmail.com>
2021-12-30 23:31:00 +0700

    change key pea_vim to lunar_vim

--- lua/pea/config/presence.lua
+++ lua/pea/config/presence.lua
@@ -10,14 +10,14 @@ M.config = function()
     local client_id = {
         vs_code = "383226320970055681",
         coc_nvim = "768090036633206815",
-        pea_vim = "914799712794705961",
+        lunar_vim = "914799712794705961",
     }
 
     presence:setup {
         auto_update = true,
         neovim_image_text = "NeoVim, not Vim pls!",
         main_image = "file",
-        client_id = client_id.pea_vim,
+        client_id = client_id.lunar_vim,
         buttons = true,
         workspace_text = function()
             return "Rustacean"
