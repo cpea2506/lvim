7ce73e0e438c3389c462ad172bc15726ec8b85cf
cpea2506 <vienpro2506@gmail.com>
2021-12-31 00:25:18 +0700

    turn off vim regex highlighting

--- lua/pea/treesitter.lua
+++ lua/pea/treesitter.lua
@@ -1,7 +1,9 @@
+-- lvim.builtin.treesitter.sync_install = true
 -- lvim.builtin.treesitter.ensure_installed = "maintained"
 lvim.builtin.treesitter.autotag.enable = true
 lvim.builtin.treesitter.highlight.enable = true
 lvim.builtin.treesitter.rainbow.enable = true
+lvim.builtin.treesitter.highlight.additional_vim_regex_highlighting = false
 lvim.builtin.treesitter.rainbow.colors = {
     "#e492e1",
     "#fed701",
