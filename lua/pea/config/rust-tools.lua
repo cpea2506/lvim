191bcc4dca0adb7d15d576606d529fc33c11d4fa
cpea2506 <vienpro2506@gmail.com>
2022-01-06 02:33:00 +0700

    rust-tool: config add more options

--- lua/pea/config/rust-tools.lua
+++ lua/pea/config/rust-tools.lua
@@ -7,12 +7,55 @@ M.config = function()
         return
     end
 
-    rust_tools.setup {
+    local config = {
         tools = {
             autoSetHints = true,
             hover_with_actions = true,
             runnables = {
                 use_telescope = true,
+            },
+            inlay_hints = {
+                -- Only show inlay hints for the current line
+                only_current_line = false,
+                -- Event which triggers a refersh of the inlay hints.
+                -- You can make this "CursorMoved" or "CursorMoved,CursorMovedI" but
+                -- not that this may cause  higher CPU usage.
+                -- This option is only respected when only_current_line and
+                -- autoSetHints both are true.
+                only_current_line_autocmd = "CursorHold",
+                -- wheter to show parameter hints with the inlay hints or not
+                show_parameter_hints = true,
+                -- prefix for parameter hints
+                parameter_hints_prefix = "<- ",
+                -- prefix for all the other hints (type, chaining)
+                other_hints_prefix = "=> ",
+                -- whether to align to the length of the longest line in the file
+                max_len_align = false,
+                -- padding from the left if max_len_align is true
+                max_len_align_padding = 1,
+                -- whether to align to the extreme right or not
+                right_align = false,
+                -- padding from the right if right_align is true
+                right_align_padding = 7,
+                -- The color of the hints
+                highlight = "Comment",
+            },
+            hover_actions = {
+                -- the border that is used for the hover window
+                -- see vim.api.nvim_open_win()
+                border = {
+                    { "╭", "FloatBorder" },
+                    { "─", "FloatBorder" },
+                    { "╮", "FloatBorder" },
+                    { "│", "FloatBorder" },
+                    { "╯", "FloatBorder" },
+                    { "─", "FloatBorder" },
+                    { "╰", "FloatBorder" },
+                    { "│", "FloatBorder" },
+                },
+
+                -- whether the hover action window gets automatically focused
+                auto_focus = true,
             },
         },
         server = {
@@ -21,13 +64,20 @@ M.config = function()
             on_init = require("lvim.lsp").common_on_init,
             settings = {
                 ["rust-analyzer"] = {
+                    diagnostics = {
+                        enable = true,
+                    },
                     checkOnSave = {
+                        enable = true,
                         command = "clippy",
                     },
                 },
             },
+            standalone = false,
         },
     }
+
+    rust_tools.setup(config)
 end
 
 return M
