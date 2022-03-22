c447de23f52f246b20d7656df432ca35c82c8e8c
Do Vien <vienpro2506@gmail.com>
2022-03-22 17:33:06 +0700

    config: enable switch number modes autocommand

--- config.lua
+++ config.lua
@@ -53,7 +53,7 @@ lvim.builtin.bufferline.options.show_close_icon = true
 -- lualine
 lvim.builtin.evil_lualine = { active = true }
 if lvim.builtin.evil_lualine.active then
-    require("pea.config.evil_lualine").config()
+    require("pea.config.evil_lualine").setup()
 end
 
 -- autocmd
@@ -63,6 +63,6 @@ lvim.autocommands.custom_groups = {
     -- On entering insert mode in any file, scroll the window so the cursor line is centered
     { "InsertEnter", "*", ":normal zz" },
     -- switch line number modes between normal and other vim modes
-    -- { "BufEnter,FocusGained,InsertLeave,WinEnter", "*", "if &nu && mode() != 'i' | set rnu | endif" },
-    -- { "BufLeave,FocusLost,InsertEnter,WinLeave", "*", "if &nu | set nornu | endif" },
+    { "BufEnter,FocusGained,InsertLeave,WinEnter", "*", "if &nu && mode() != 'i' | set rnu | endif" },
+    { "BufLeave,FocusLost,InsertEnter,WinLeave", "*", "if &nu | set nornu | endif" },
 }
