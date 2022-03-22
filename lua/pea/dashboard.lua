dcdf6b0bf2505c1126bca63cdb691c4fbca9b8c3
Do Vien <vienpro2506@gmail.com>
2022-03-22 17:34:41 +0700

    dashboard: refactor to new alpha setting

--- lua/pea/dashboard.lua
+++ lua/pea/dashboard.lua
@@ -1,5 +1,6 @@
-lvim.builtin.dashboard.active = true
-lvim.builtin.dashboard.custom_header = {
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.alpha.dashboard.section.header.val = {
     "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
     "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⡿⣟⣶⣿⣧⣿⣿⣦⢿⣿⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
     "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣏⣿⣿⣿⣿⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
@@ -24,3 +25,32 @@ lvim.builtin.dashboard.custom_header = {
     "⣿⣿⠀⠀⠀⠀⣰⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣆⣀⣠⣴⣿⡿⣿⣿⣤⣿⣿⣿⣇⣶⣿⡿⠁⠀⠀⠀⠀⢸⣿⠀⠀⢀⣾⡿⠛⠀⠀⠸⡿⣇⣴⣿⣿⠃⠀⠀⣼⡿⠟⠀⠀⠀⣿⣿⣠⣾⣿⠋⠀",
     "⠀⠻⢷⣶⣾⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠉⠉⠉⠉  ⠀⠈⠉⠉⠀⠀⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⣿⠿⠛⠀⠀⠀⠀⠀ ⠉⠉⠉⠉⠀⠀⠀⠀⠉⠀⠀⠀⠀⠀⠈⠉⠉⠀⠀⠀",
 }
+lvim.builtin.alpha.dashboard.section.header.opts.hl = "DashBoardHeader"
+lvim.builtin.alpha.dashboard.section.footer.opts.hl = "DashBoardFooter"
+
+local text = require "lvim.interface.text"
+local packer_path = vim.fn.stdpath "data" .. "/site/pack/packer/start"
+local num_plugins_loaded = #vim.fn.globpath(packer_path, "*", 0, 1)
+
+lvim.builtin.alpha.dashboard.section.footer.val = text.align_center({ width = 0 }, {
+    "",
+    "PeaVim loaded " .. num_plugins_loaded .. " plugins ",
+    "",
+    "Let me show you what is true text editor!",
+}, 0.5)
+
+local dashboard = require "alpha.themes.dashboard"
+lvim.builtin.alpha.dashboard.section.buttons.val = {}
+
+for _, entry in pairs(lvim.builtin.alpha.dashboard.section.buttons.entries) do
+    local on_press = function()
+        local sc_ = entry[1]:gsub("%s", ""):gsub("SPC", "<leader>")
+        local key = vim.api.nvim_replace_termcodes(sc_, true, false, true)
+        vim.api.nvim_feedkeys(key, "normal", false)
+    end
+
+    local button_element = dashboard.button(entry[1], entry[2], entry[3])
+    button_element.on_press = on_press
+    button_element.opts.hl = "DashBoardCenter"
+    table.insert(lvim.builtin.alpha.dashboard.section.buttons.val, button_element)
+end
