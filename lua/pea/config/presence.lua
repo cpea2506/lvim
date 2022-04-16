596f5182ae9580a07b2bd77a9fc17820be47ae6d
Do Vien <vienpro2506@gmail.com>
2022-04-17 00:20:40 +0700

    presence: change client_id name

--- lua/pea/config/presence.lua
+++ lua/pea/config/presence.lua
@@ -11,14 +11,14 @@ M.config = function()
         default = "793271441293967371",
         vs_code = "383226320970055681",
         coc_nvim = "768090036633206815",
-        lunar_vim = "914799712794705961",
+        pea_vim = "914799712794705961",
     }
 
     presence:setup {
         auto_update = true,
         neovim_image_text = "NeoVim Chick!",
         main_image = "file",
-        client_id = client_id.lunar_vim,
+        client_id = client_id.pea_vim,
         buttons = true,
         workspace_text = function()
             return "Rustacean"
