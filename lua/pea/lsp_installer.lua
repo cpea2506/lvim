e6ff35fe3810efa22753fc0e79fc4b62f4ed2044
Do Vien <vienpro2506@gmail.com>
2022-04-29 11:25:24 +0700

    lsp_installer: using new setup function

--- lua/pea/lsp_installer.lua
+++ lua/pea/lsp_installer.lua
@@ -3,7 +3,8 @@ local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
 lvim.lsp.automatic_servers_installation = false
 
 if status_ok then
-    lsp_installer.settings {
+    lsp_installer.setup {
+        ensure_installed = { "rust_analyzer", "sumneko_lua", "taplo", "tsserver", "cssls", "jsonls" },
         ui = {
             icons = {
                 server_installed = "✓",
