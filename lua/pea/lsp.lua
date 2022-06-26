d55b2b6882d2ccf491144bf82f12e7c6b4e03fdd
Do Vien <vienpro2506@gmail.com>
2022-06-26 19:50:06 +0700

    lsp: add lsp installer setup

--- lua/pea/lsp.lua
+++ lua/pea/lsp.lua
@@ -1,36 +1,23 @@
-local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
-
-if status_ok then
-    lsp_installer.setup {
-        ensure_installed = {
-            "rust_analyzer",
-            "sumneko_lua",
-            "taplo",
-            "tsserver",
-            "cssls",
-            "jsonls",
-        },
-        ui = {
-            border = "rounded",
-            icons = {
-                server_installed = "✓",
-                server_pending = "",
-                server_uninstalled = "✗",
-            },
-            keymaps = {
-                toggle_server_expand = "o",
-                install_server = "i",
-                update_server = "u",
-                uninstall_server = "d",
-            },
-        },
-    }
-end
-
-local opts = {
+local lsp = {
     automatic_servers_installation = false,
-    float = {
-        focusable = true,
+    installer = {
+        setup = {
+            ensure_installed = {
+                "rust_analyzer",
+                "sumneko_lua",
+                "taplo",
+                "tsserver",
+                "cssls",
+                "jsonls",
+            },
+            ui = {
+                border = "rounded",
+                keymaps = {
+                    toggle_server_expand = "o",
+                    uninstall_server = "d",
+                },
+            },
+        },
     },
     buffer_mappings = {
         normal_mode = {
@@ -54,4 +41,4 @@ local opts = {
     },
 }
 
-return opts
+return lsp
