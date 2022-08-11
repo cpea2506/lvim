fec9ad754fc95ab0858b89ef3e3833e03182cd53
CPea <vienpro2506@gmail.com>
2022-08-11 12:46:47 +0700

    chore(inlay-hints): update config

--- lua/pea/config/inlay_hints.lua
+++ lua/pea/config/inlay_hints.lua
@@ -2,9 +2,10 @@ return function()
     local status_ok, inlay_hints = pcall(require, "inlay-hints")
 
     if not status_ok then
-        print "cannot find inlay-hints module"
         return
     end
 
-    inlay_hints.setup()
+    inlay_hints.setup {
+        renderer = "inlay-hints.render.eol",
+    }
 end
