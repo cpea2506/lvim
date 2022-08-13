1e9f54760bf5837f3402782311bb7740bf88fb1b
CPea <vienpro2506@gmail.com>
2022-08-13 12:28:47 +0700

    feat(ftplugin): format markdown with prettier

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,1 +1,9 @@
 vim.opt_local.textwidth = 80
+
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+    {
+        command = "prettier",
+        filetypes = { "markdown" },
+    },
+}
