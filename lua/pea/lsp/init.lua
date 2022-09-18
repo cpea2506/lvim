ea56ed08f5a95e8d5df82c90ac7469891da92091
CPea <vienpro2506@gmail.com>
2022-09-18 23:08:02 +0700

    feat(lsp): move support function into utils module

---
+++ lua/pea/lsp/init.lua
@@ -0,0 +1,49 @@
+local utils = require "pea.lsp.utils"
+
+local lsp = {
+    installer = {
+        setup = {
+            automatic_installation = false,
+            ensure_installed = {
+                "rust_analyzer",
+                "marksman",
+                "sumneko_lua",
+                "taplo",
+                "emmet_ls",
+                "svelte",
+                "tailwindcss",
+                "yamlls",
+                "bashls",
+                "tsserver",
+                "cssls",
+                "jsonls",
+            },
+        },
+    },
+    buffer_mappings = {
+        normal_mode = {
+            ["K"] = {
+                utils.show_documentation,
+                "Show hover",
+            },
+        },
+    },
+    on_attach_callback = utils.on_attach,
+    diagnostics = {
+        virtual_text = true,
+        update_in_insert = true,
+        float = {
+            focusable = true,
+        },
+        signs = {
+            values = {
+                { name = "DiagnosticSignError", text = "" },
+                { name = "DiagnosticSignWarn", text = "" },
+                { name = "DiagnosticSignHint", text = "" },
+                { name = "DiagnosticSignInfo", text = "" },
+            },
+        },
+    },
+}
+
+return lsp
