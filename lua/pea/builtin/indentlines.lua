976239785e86fcda187b02520934a5e85e78b585
CPea <vienpro2506@gmail.com>
2022-10-07 19:44:25 +0700

    feat: enable indentlines for appropriate filetypes

--- lua/pea/builtin/indentlines.lua
+++ lua/pea/builtin/indentlines.lua
@@ -1,5 +1,21 @@
 local indentlines = {
-    active = false,
+    active = true,
+    options = {
+        indent_level = 10,
+        max_indent_increase = 1,
+        show_first_indent_level = false,
+        strict_tabs = true,
+        filetype = { "svelte", "typescriptreact", "html" },
+        use_treesitter_scope = true,
+        char_highlight_list = {
+            "IndentBlanklineIndent1",
+            "IndentBlanklineIndent2",
+            "IndentBlanklineIndent3",
+            "IndentBlanklineIndent4",
+            "IndentBlanklineIndent5",
+            "IndentBlanklineIndent6",
+        },
+    },
 }
 
 return indentlines
