e40a50b6e21203a85ffc392ef82eab73596588cf
CPea <vienpro2506@gmail.com>
2022-11-19 15:37:50 +0700

    feat(dressing): allow normal mode on input

--- lua/pea/config/dressing.lua
+++ lua/pea/config/dressing.lua
@@ -1,18 +1,22 @@
 return function()
     require("dressing").setup {
         input = {
-            enabled = true,
             winblend = 0,
             anchor = "NW",
+            insert_only = false,
             override = function(conf)
                 conf.col = 1
                 conf.row = 1
 
                 return conf
             end,
+            mappings = {
+                n = {
+                    ["q"] = "Close",
+                },
+            },
         },
         select = {
-            enabled = true,
             backend = { "telescope" },
         },
     }
