28e4d853c785c8ca7fd5cafeaf3c2a2d2760e975
CPea <vienpro2506@gmail.com>
2022-12-20 21:33:46 +0700

    feat: add support for swift

--- after/ftplugin/swift.lua
+++ after/ftplugin/swift.lua
@@ -1,7 +1,17 @@
+require("lvim.lsp.manager").setup "sourcekit"
+
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
     {
         command = "swiftformat",
+        filetypes = { "swift" },
+    },
+}
+
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
+    {
+        command = "swiftlint",
         filetypes = { "swift" },
     },
 }
