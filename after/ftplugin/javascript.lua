c59af31a63e55cbfb7f6714e62de2e937c33d909
CPea <vienpro2506@gmail.com>
2023-01-25 16:48:18 +0700

    feat: add formatter and linter for js file

---
+++ after/ftplugin/javascript.lua
@@ -0,0 +1,15 @@
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+    {
+        command = "prettierd",
+        filetypes = { "javascript" },
+    },
+}
+
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
+    {
+        command = "eslint_d",
+        filetypes = { "javascript" },
+    },
+}
