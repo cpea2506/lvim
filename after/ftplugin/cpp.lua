ae409efd5725bdc9eb475b44a9c55b880067c542
CPea <vienpro2506@gmail.com>
2023-02-20 17:02:24 +0700

    feat(ftplugin/cpp): add formatter, linter and setup server

---
+++ after/ftplugin/cpp.lua
@@ -0,0 +1,35 @@
+-- some settings can only passed as commandline flags, see `clangd --help`
+local clangd_flags = {
+    "--background-index",
+    "--all-scopes-completion",
+    "--clang-tidy",
+    "--log=error",
+    "--suggest-missing-includes",
+    "--cross-file-rename",
+    "--completion-style=detailed",
+    "--pch-storage=memory", -- could also be disk
+    "--folding-ranges",
+    "--enable-config", -- clangd 11+ supports reading from .clangd configuration file
+    "--offset-encoding=utf-16", --temporary fix for null-ls
+    "--clang-tidy-checks=-*,llvm-*,clang-analyzer-*,modernize-*,-modernize-use-trailing-return-type",
+}
+
+require("lvim.lsp.manager").setup("clangd", {
+    cmd = { "clangd", unpack(clangd_flags) },
+})
+
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+    {
+        command = "clang-format",
+        filetypes = { "cpp" },
+    },
+}
+
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
+    {
+        command = "cppcheck",
+        filetypes = { "cpp" },
+    },
+}
