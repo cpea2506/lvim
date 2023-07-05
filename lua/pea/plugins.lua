0aa9f1a4ccb6557f6cd345e2d94a15c254bc5a5b
CPea <cpea2506@gmail.com>
2023-07-05 16:52:57 +0700

    feat(plugins): replace `nvim-ts-rainbow2` with `rainbow-delimiters`

--- lua/pea/plugins.lua
+++ lua/pea/plugins.lua
@@ -24,7 +24,7 @@ local plugins = {
         config = require "pea.config.gx",
     },
     {
-        "HiPhish/nvim-ts-rainbow2",
+        "HiPhish/rainbow-delimiters.nvim",
         event = "BufRead",
     },
     {
