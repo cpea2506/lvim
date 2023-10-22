c716eecbed98e80419def187b968d7943fdcc154
CPea <cpea2506@gmail.com>
2023-10-23 02:57:33 +0700

    feat(plugins/rustaceanvim): no autofocus on hover

--- lua/pea/config/rustacean.lua
+++ lua/pea/config/rustacean.lua
@@ -2,9 +2,6 @@ return function()
     vim.g.rustaceanvim = {
         tools = {
             executor = "toggleterm",
-            hover_actions = {
-                auto_focus = true,
-            },
         },
     }
 end
