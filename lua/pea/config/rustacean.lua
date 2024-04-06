b9ddf4c727b7a8ecc84f505a79152adc9a857df5
CPea <cpea2506@gmail.com>
2024-04-06 21:54:29 +0700

    fix(rust): `gd` as go to definition key

--- lua/pea/config/rustacean.lua
+++ lua/pea/config/rustacean.lua
@@ -3,5 +3,10 @@ return function()
         tools = {
             executor = "toggleterm",
         },
+        server = {
+            on_attach = function()
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+            end,
+        },
     }
 end
